8df5a3a5a050565afe30ec136a6424145bfb1bca
Sudheeradh <95284503+Sudheeradh@users.noreply.github.com>
2022-11-03 01:35:40 +0530

    Time: 345 ms (41.27%), Space: 15 MB (56.28%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,19 +1,30 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        goal = len(nums) - 1
-        ptr = goal
-        min_pos_ptr = goal
-        steps = 0
+#         goal = len(nums) - 1
+#         ptr = goal
+#         min_pos_ptr = goal
+#         steps = 0
         
-        while goal > 0:
-            while ptr > 0:
-                ptr -= 1
-                if goal - ptr <= nums[ptr]:
-                    min_pos_ptr = min(ptr, min_pos_ptr)
-            steps += 1
-            goal = min_pos_ptr
-            ptr = goal
+#         while goal > 0:
+#             while ptr > 0:
+#                 ptr -= 1
+#                 if goal - ptr <= nums[ptr]:
+#                     min_pos_ptr = min(ptr, min_pos_ptr)
+#             steps += 1
+#             goal = min_pos_ptr
+#             ptr = goal
         
-        return steps
+#         return steps
+
+        l, r = 0, 0
+        res = 0
+        while r < len(nums) - 1:
+            farthest = 0
+            for i in range(l, r + 1):
+                farthest = max(farthest, i + nums[i])
+            l = r + 1
+            r = farthest
+            res += 1
+        return res
         
         
