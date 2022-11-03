092ea40d3031ad5f1a539f624142e00a8f40c331
Sudheeradh <95284503+Sudheeradh@users.noreply.github.com>
2022-11-04 04:12:54 +0530

    Time: 394 ms (58.15%), Space: 15.2 MB (55.99%) - LeetHub

--- 0665-non-decreasing-array/0665-non-decreasing-array.py
+++ 0665-non-decreasing-array/0665-non-decreasing-array.py
@@ -9,11 +9,11 @@ class Solution:
                     return False
             return True
         
-        nums.append(float('inf'))
-        nums.insert(0, float('-inf'))
-        
         if check(nums):
             return True
+        
+        nums.append(float('inf'))
+        nums.insert(0, float('-inf'))
         
         conflict = 0
         for i in range(len(nums) - 1):
@@ -21,16 +21,10 @@ class Solution:
                 conflict = i
                 break
         
-        main1 = nums[conflict - 1 : conflict + 3]
-        main2 = nums[conflict - 1 : conflict + 3]
-        main1[1] = main1[0]
-        main2[2] = main2[3]
-        
-        if check(main1):
-            nums[conflict - 1 : conflict + 3] = main1
-        
-        elif check(main2):
-            nums[conflict - 1 : conflict + 3] = main2
+        if nums[conflict] <= nums[conflict + 2]:
+            nums[conflict + 1] = nums[conflict + 2]
+        else:
+            nums[conflict] = nums[conflict - 1]
         
         return check(nums)
         
