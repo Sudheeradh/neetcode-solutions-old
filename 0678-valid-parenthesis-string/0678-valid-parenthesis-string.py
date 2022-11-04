c1a97de5f39a4b41b4b06dc62c620d568287dcc6
Sudheeradh <95284503+Sudheeradh@users.noreply.github.com>
2022-11-04 13:03:40 +0530

    Time: 24 ms (99.27%), Space: 14 MB (14.67%) - LeetHub

---
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.py
@@ -0,0 +1,20 @@
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        left_min = 0
+        left_max = 0
+        for i in s:
+            if i == "(":
+                left_min += 1
+                left_max += 1
+            elif i == "*":
+                left_min = max(0, left_min - 1)
+                left_max += 1
+            else:
+                left_max -= 1
+                left_min = max(0, left_min - 1)
+                if left_max < 0:
+                    return False
+        if left_min > 0  or left_max < 0:
+            return False
+        return True
+            
