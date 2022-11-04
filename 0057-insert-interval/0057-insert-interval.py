de67fa26b77b8db77659ebffe4d25e72caa84e00
Sudheeradh <95284503+Sudheeradh@users.noreply.github.com>
2022-11-04 15:39:13 +0530

    Time: 88 ms (90.12%), Space: 17.5 MB (24.09%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -8,20 +8,17 @@ class Solution:
         def overlap(interval_a, interval_b):
             return not (interval_a[1] < interval_b[0] or interval_b[1] < interval_a[0])
         
-        mod = True
-        
         for i in range(len(intervals)):
             if not overlap(intervals[i], newInterval):
-                if newInterval[1] < intervals[i][0] and mod:
+                if newInterval[1] < intervals[i][0]:
                     res.append(newInterval)
-                    mod = False
+                    return res + intervals[i:]
                 res.append(intervals[i])
             else:
                 newInterval[0] = min(intervals[i][0], newInterval[0])
                 newInterval[1] = max(intervals[i][1], newInterval[1])
-                if i == len(intervals) - 1 and mod:
+                if i == len(intervals) - 1:
                     res.append(newInterval)
-                    mod = False
         
         if intervals[-1][1]  < newInterval[0]:
             res.append(newInterval)
