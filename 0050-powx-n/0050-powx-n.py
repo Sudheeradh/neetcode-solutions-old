58fb62ad5d105621ee368b7ecf527fa7f657445f
Sudheeradh <95284503+Sudheeradh@users.noreply.github.com>
2022-11-05 10:13:04 +0530

    Time: 54 ms (60.87%), Space: 13.9 MB (20.25%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.py
@@ -0,0 +1,9 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        
+#         def power()
+        
+#         if (abs(n) % 2 != 0):
+        return pow(x, n)
+            
+        
