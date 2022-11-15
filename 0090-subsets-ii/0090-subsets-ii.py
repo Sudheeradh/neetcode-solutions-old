f6d2de0bba56fc8f3f2ba0ff20168b6b97e85c46
Sudheeradh <95284503+Sudheeradh@users.noreply.github.com>
2022-11-15 14:51:16 +0530

    Time: 72 ms (54.59%), Space: 14.1 MB (50.17%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.py
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -1,10 +1,12 @@
+from bisect import bisect_left
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
         curr = []
         res = [[]]
+        nums.sort()
         
         def _swd(ptr):
-            if ptr == len(nums):
+            if ptr >= len(nums):
                 return
             
             curr.append(nums[ptr])
@@ -12,14 +14,14 @@ class Solution:
             _swd(ptr + 1)
             
             curr.pop()
-            _swd(ptr + 1)
+            _swd(bisect_left(nums,nums[ptr] + 1))
         
         _swd(0)
         
-        for i in range(len(res)):
-            res[i] = tuple(sorted(res[i]))
+#         for i in range(len(res)):
+#             res[i] = tuple(sorted(res[i]))
         
-        res = list(set(res))
+#         res = list(set(res))
         return res
         
         
