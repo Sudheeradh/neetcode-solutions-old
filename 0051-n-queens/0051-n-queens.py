b32447ae82a1294247cea0ddebc5eccc89e599d0
Sudheeradh <95284503+Sudheeradh@users.noreply.github.com>
2022-11-16 06:39:57 +0530

    Time: 73 ms (88.25%), Space: 14.5 MB (13.23%) - LeetHub

--- 0051-n-queens/0051-n-queens.py
+++ 0051-n-queens/0051-n-queens.py
@@ -4,32 +4,31 @@ class Solution:
         posDiag = set()
         negDiag = set()
         
-        res = []
         board = [['.'] * n for i in range(n)]
+        res = []
         
         def backtrack(r):
-            if r == n:
+            if (r == n):
                 res.append(["".join(row) for row in board])
                 return
             
             for c in range(n):
-                if ((c in col) or
+                if ((c in col) or 
                    ((r + c) in posDiag) or
                    ((r - c) in negDiag)):
                     continue
-            
+                
                 board[r][c] = "Q"
                 col.add(c)
                 posDiag.add(r + c)
                 negDiag.add(r - c)
-
+                
                 backtrack(r + 1)
-
+                
                 board[r][c] = "."
                 col.remove(c)
                 posDiag.remove(r + c)
                 negDiag.remove(r - c)
-        
+            
         backtrack(0)
         return res
-        
